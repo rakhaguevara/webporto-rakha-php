1e4f309548c4d980293b796d3be74e936f3b9d2c
rakhaPC <rakha.guevara2505@gmail.com>
2025-12-29 22:19:01 +0700

    "sql 3 terbaru"

---
+++ webporto (3).sql
@@ -0,0 +1,160 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 29 Des 2025 pada 16.18
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `webporto`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(11) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `password`) VALUES
+(1, 'rakha', 'rakha123');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `id_kategori` int(11) NOT NULL,
+  `nama_kategori` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `kategori`
+--
+
+INSERT INTO `kategori` (`id_kategori`, `nama_kategori`) VALUES
+(1, 'project_pemerintah'),
+(2, 'Project Pribadi');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `photos`
+--
+
+CREATE TABLE `photos` (
+  `id` int(11) NOT NULL,
+  `judul_karya` varchar(255) NOT NULL,
+  `deksripsi_foto` varchar(255) NOT NULL,
+  `gambar` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `project`
+--
+
+CREATE TABLE `project` (
+  `id_project` int(11) NOT NULL,
+  `kategori_project` int(11) NOT NULL,
+  `judul_project` varchar(255) NOT NULL,
+  `deksripsi` text NOT NULL,
+  `tanggal_pembuatan` datetime NOT NULL,
+  `gambar` varchar(255) NOT NULL,
+  `link_project` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `project`
+--
+
+INSERT INTO `project` (`id_project`, `kategori_project`, `judul_project`, `deksripsi`, `tanggal_pembuatan`, `gambar`, `link_project`) VALUES
+(1, 2, 'Web porto pertama', 'ini adalah web porto pertama aku di tahu 2025', '2023-12-13 16:54:45', 'portfolio-5.jpg', 'https://rakhaguevara.github.io/portofoliolama.github.io/'),
+(2, 1, 'Aplikasi Spark', 'Spark-Smart Parking Application For Reservation And Keeper', '2025-12-29 15:27:24', 'portfolio-4.jpg', 'test.com'),
+(3, 1, 'Aplikasi Spark', 'Spark-Smart Parking Application For Reservation And Keeper', '2025-12-29 15:27:24', 'portfolio-4.jpg', 'test.com'),
+(4, 1, 'Aplikasi Spark', 'Spark-Smart Parking Application For Reservation And Keeper', '2025-12-29 15:27:24', 'portfolio-4.jpg', 'test.com');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indeks untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indeks untuk tabel `project`
+--
+ALTER TABLE `project`
+  ADD PRIMARY KEY (`id_project`),
+  ADD KEY `fk_project_kategori` (`kategori_project`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `project`
+--
+ALTER TABLE `project`
+  MODIFY `id_project` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `project`
+--
+ALTER TABLE `project`
+  ADD CONSTRAINT `fk_project_kategori` FOREIGN KEY (`kategori_project`) REFERENCES `kategori` (`id_kategori`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
